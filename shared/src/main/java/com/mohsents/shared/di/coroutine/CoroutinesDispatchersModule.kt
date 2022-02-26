76fb353f6d4eb22164a809dfff5d9f2a31af6ef0
Mohsents <mohsenteimury2000@gmail.com>
2022-02-26 18:35:38 +0330

    Provide Dispatchers with @Singleton

--- shared/src/main/java/com/mohsents/shared/di/coroutine/CoroutinesDispatchersModule.kt
+++ shared/src/main/java/com/mohsents/shared/di/coroutine/CoroutinesDispatchersModule.kt
@@ -22,6 +22,8 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
@@ -29,17 +31,37 @@ object CoroutinesDispatchersModule {
 
     @DefaultDispatcher
     @Provides
+    @Singleton
     fun providesDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
 
     @IoDispatcher
     @Provides
+    @Singleton
     fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
 
     @MainDispatcher
     @Provides
+    @Singleton
     fun providesMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
 
     @MainImmediateDispatcher
     @Provides
+    @Singleton
     fun providesMainImmediateDispatcher(): CoroutineDispatcher = Dispatchers.Main.immediate
 }
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class DefaultDispatcher
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class IoDispatcher
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class MainDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class MainImmediateDispatcher
